3155920c0106d4215d9495741bdf2a753b4edf33
JuanRudas02 <132242179+JuanRudas02@users.noreply.github.com>
2023-07-17 22:21:38 -0500

    First proyect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import img from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
+import Characters from './componets/Characters';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+  const reactApi = async () =>{
+    const api =  await fetch('https://rickandmortyapi.com/api/character')
+    const characterApi = await api.json();
+
+   
+
+    setCharacters(characterApi.results);
+  }
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className='title'> Rick and Morty</h1>
+        {characters ?(
+          <Characters  characters={characters} setCharacters={setCharacters}/>
+
+        ): 
+        
+        (
+          <>
+          
+          <img src={img} alt="Rick and Morty " className='img-home' />
+        
+          <button onClick={ reactApi} className='btn-search'>Buscar personaje</button>
+          </>
+        )}
+        
+
+        
+       </header>
+       </div>
   );
 }
 
---
+++ src/componets/Characters.js
@@ -0,0 +1,61 @@
+
+
+export default function Characters(props) {
+  const {characters,setCharacters} = props;
+
+  const resetCharacters = () =>{
+
+      setCharacters(null);
+  }
+
+  return (
+    <div className="characters">
+
+      <h1>Laura</h1>
+      <span className="back-home" onClick={resetCharacters}> Volver al home</span>
+      <div className="container-characters">
+        {characters.map((Characters,index)=>(
+          <div className="character-container" key={index}> 
+             <div> 
+
+              <img src={Characters.image} alt={Characters.name} />
+
+             </div>
+              <div>
+              <h3>{Characters.name}</h3> 
+              <h6>{Characters.status === "Alive" ?(
+
+                <>
+                <span className="alive" />
+                Alive
+                </>
+              ) :
+                <>
+                  <span className="dead" />
+                  Dead
+                </>
+              
+              }</h6>
+              <p>
+              <span className="text-gray"> Episodios: </span>
+              <span>{Characters.episode.length}</span>
+              </p>
+              <p>
+              <span className="text-gray"> Epecie: </span>
+              <span>{Characters.species}</span>
+              </p>
+              </div>
+              
+          </div>
+
+        ))} 
+
+
+      </div>
+      <span className="back-home" onClick={resetCharacters}>Regresar a la home </span>
+      <h1 className="back-home">Esta pagina esta hecha por Juan Rudas </h1>
+      <h1 className="back-home">Fecha: 17/07//2023 </h1>
+
+    </div>
+  )
+}
